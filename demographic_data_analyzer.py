9602473e2ec1f09b8c546224c7967d4989a5e960
Muhammad Faris Herlansyah <44311504+mfarisherlansyah@users.noreply.github.com>
2022-06-12 12:03:53 +0000

    First commit on demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,42 +3,42 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.groupby("race").size().sort_values(ascending = False)
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df["sex"] == "Male"]["age"].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round((df.groupby("education").size()["Bachelors"] / df.groupby("education").size().sum()) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = (df.groupby("education").size()[["Bachelors", "Masters", "Doctorate"]].sum())
+    lower_education = df.groupby("education").size().sum() - higher_education
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round((df[df["salary"] == df["salary"].unique()[1]].groupby("education").size()[["Bachelors", "Masters", "Doctorate"]].sum() / higher_education) * 100, 1)
+    lower_education_rich = round((df[df["salary"] == df["salary"].unique()[1]].groupby("education").size()[[e for e in df[df["salary"] == df["salary"].unique()[1]].groupby("education").size().index if e not in {"Bachelors", "Masters", "Doctorate"}]].sum() / lower_education) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df["hours-per-week"] == df["hours-per-week"].min()].shape[0]
 
-    rich_percentage = None
+    rich_percentage = round((df[df["hours-per-week"] == df["hours-per-week"].min()].groupby("salary").size()[1] / num_min_workers) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country = [n for n in df[df["salary"] == df["salary"].unique()[1]]["native-country"].unique()][[df[df["salary"] == df["salary"].unique()[1]].groupby('native-country').size()[n]/df.groupby('native-country').size()[n] for n in df[df["salary"] == df["salary"].unique()[1]]["native-country"].unique()].index(max([df[df["salary"] == df["salary"].unique()[1]].groupby('native-country').size()[n]/df.groupby('native-country').size()[n] for n in df[df["salary"] == df["salary"].unique()[1]]["native-country"].unique()]))]
+    highest_earning_country_percentage = round(max([df[df["salary"] == df["salary"].unique()[1]].groupby('native-country').size()[n]/df.groupby('native-country').size()[n] for n in df[df["salary"] == df["salary"].unique()[1]]["native-country"].unique()]) * 100, 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df.loc[(df["salary"] == df["salary"].unique()[1]) & (df["native-country"] == "India")].groupby("occupation").size().sort_values().index[-1]
 
     # DO NOT MODIFY BELOW THIS LINE
 
